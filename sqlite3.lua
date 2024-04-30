14a9530de483aec09b3a9e574b499ba5d74733b7
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-04-30 16:35:45 -0300

    Update sqlite3.lua

--- sqlite3.lua
+++ sqlite3.lua
@@ -1,11 +1,25 @@
-local db_path = "database.db"
+local sqlite = {}
 
-local sqlite_queries = {
-  [[".read create_music_library.sql"]],
-  [[".read insert_musics.sql"]]
+function sqlite.query_format(query)
+  local formated_query = ' "' .. query .. '"'
+  return formated_query
+end
+
+function sqlite:cli_execute(query)
+  local quoted_query = self.query_format(query)
+  local command = self.connector .. quoted_query
+  return os.execute(command)
+end
+
+sqlite.connector = "sqlite3 music.db"
+sqlite.queries = {
+  ".read music_library.sql",
+  ".read insert_musics.sql"
 }
 
-for _, query in ipairs(sqlite_queries) do
-  local cmd = "sqlite3" .. " " .. db_path .. " " .. query
-  os.execute(cmd)
+for _, query in ipairs(sqlite.queries) do
+  local success, exit_code, signal = sqlite:cli_execute(query)
+  if not success then
+    print("Sqlite Error: " .. query)
+  end
 end
