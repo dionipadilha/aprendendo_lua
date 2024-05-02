d39156152aeb186315b47338dd73842c7efa43b4
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-02 16:24:20 -0300

    Create table_move2.lua

---
+++ table_move2.lua
@@ -0,0 +1,7 @@
+-- Define source and target tables:
+local source = { 1, 2, 3, 4 }
+local target = { "a", "b", "c", "d" }
+
+-- Move elements from the source to the target table:
+table.move(source, 1, #source, 1, target)
+print(table.concat(target, ", ")) --> 1, 2, 3, 4
