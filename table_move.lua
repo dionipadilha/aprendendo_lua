330f7019de0fc31fb30da422c3ed2c1588f507b8
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-02 16:33:49 -0300

    Update table_move.lua

--- table_move.lua
+++ table_move.lua
@@ -6,4 +6,6 @@ local target = { "a", "b", "c", "d" }
 
 -- Move elements from the source to the target table:
 table.move(source, 1, #source, 2, target)
+
+-- Print target table with moved elements:
 print(table.concat(target, ", ")) --> a, 1, 2, 3, 4
