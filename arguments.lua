b15737cdee621308ef2a85b8977aa5d3002ee73f
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-03 11:35:31 -0300

    Update arguments.lua

--- arguments.lua
+++ arguments.lua
@@ -22,12 +22,12 @@ greet("Bob", "Hey") --> Hey Bob
 
 -- Required arguments:
 local function greet(name, greeting)
-  assert(name, "name is required")
-  assert(greeting, "greeting is required")
+  assert(name, "required name argument")
+  assert(greeting, "required greeting argument")
   print(greeting .. " " .. name)
 end
 greet("Ana", "Hi") --> Hi Ana
--- greet("Bob") --> error: name and greeting are required
+-- greet("Bob")    --> error: name and greeting are required
 
 -- Default arguments:
 local function greet(name, greeting)
@@ -52,22 +52,24 @@ greet("Ana", "Bob") --> Hi Ana, Hi Bob
 -- Mixing regular and variable number of arguments:
 local function greet(greeting, ...)
   local names = { ... }
+  assert(#names > 0, "error: at least one name is required")
   for _, name in ipairs(names) do
     print(greeting .. " " .. name)
   end
 end
+--greet() --> error: at least one name is required
 greet("Hi", "Ana")         --> Hi Ana
 greet("Hey", "Ana", "Bob") --> Hey Ana, Hey Bob
 
 -- Named arguments:
 local function greet(args)
-  local name = args.name
-  local greeting = args.greeting
-  print(greeting .. " " .. name)
+  args = args or { name = "Ana", greeting = "Hi" }
+  print(args.greeting .. " " .. args.name)
 end
-greet { name = "Ana", greeting = "Hi", } --> Hi Ana
-greet { name = "Bob", greeting = "Hey", } --> Hey Bob
-greet { greeting = "Bye", name = "Jhon" } --> Bye Jhon
+greet()                                    --> Hi Ana
+greet({ name = "Bob", greeting = "Hey" })  --> Hey Bob
+greet({ greeting = "Bye", name = "Jhon" }) --> Bye Jhon
+
 
 -- Functions as arguments (Higher-Order Functions):
 local function greet(name, callback)
