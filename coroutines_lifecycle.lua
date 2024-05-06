251a0a6a34558ed3bad79678ec4a07178775c55b
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-06 12:55:40 -0300

    Update coroutines_lifecycle.lua

--- coroutines_lifecycle.lua
+++ coroutines_lifecycle.lua
@@ -16,8 +16,12 @@ print(coroutine.status(co)) --> suspended
 
 -- Resumes the coroutine until it's finished:
 repeat
-  local sucess, response = coroutine.resume(co)
-  print(sucess, response)
+  local success, response = coroutine.resume(co)
+  if not success then
+    print("Error:", response)
+    break
+  end
+  print(success, response)
 until coroutine.status(co) == "dead"
 
 print(coroutine.status(co)) --> dead
