e01fc827c60bf6874c9d7c09bc130423a3c63644
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-07 08:39:17 -0300

    Update next.lua

--- next.lua
+++ next.lua
@@ -1,6 +1,7 @@
 -- next.lua
 
 -------------------------------------------------------
+-- returns the next key-value pair in the table:
 local fruits = { "apple", "banana", "orange" }
 
 local index, value = next(fruits)
@@ -11,22 +12,19 @@ print(index, value) --> 2	banana
 
 
 index, value = next(fruits, index)
-print(index, value) --> 2	banana
+print(index, value) --> 3	orange
 
 index, value = next(fruits, index)
 print(index, value) --> nil	nil
 
 -------------------------------------------------------
-local fruits = { "apple", "banana", "orange" }
+-- while loop using next:
+local names = { "ana", "bob", "charlie" }
 
-local index, value = next(fruits)
-while index do
-  print(value)
-  index, value = next(fruits, index)
+local i, name = next(names)
+while i do
+  print(name)
+  i, name = next(names, i)
 end
 
 -------------------------------------------------------
-local fruits = { "apple", "banana", "orange" }
-for _, fruit in next, fruits do
-  print(fruit)
-end
