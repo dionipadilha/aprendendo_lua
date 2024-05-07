900c81b25d25e777624df401fec6f9786d68389b
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-07 09:51:13 -0300

    Update iterators.lua

--- iterators.lua
+++ iterators.lua
@@ -2,7 +2,7 @@
 
 -------------------------------------------------------
 -- returns the next value in the table:
-local function iterator(t)
+local function list_iterator(t)
   local i = 0
   return function()
     i = i + 1
@@ -12,12 +12,12 @@ end
 
 -------------------------------------------------------
 local fruits = { "apple", "banana", "cherry" }
-local next_fruit = iterator(fruits)
+local next_fruit = list_iterator(fruits)
 print(next_fruit()) --> apple
 print(next_fruit()) --> banana
 print(next_fruit()) --> cherry
 print(next_fruit()) -->
 
 -------------------------------------------------------
-local names = iterator({ "ana", "bob", "charlie" })
+local names = list_iterator({ "ana", "bob", "charlie" })
 for name in names do print(name) end
