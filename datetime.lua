a726596f08d11c423f73ffd17cf503b3734778e6
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-08 08:20:44 -0300

    Update datetime.lua

--- datetime.lua
+++ datetime.lua
@@ -1,32 +1,30 @@
 -- datetime.lua
 
--- Getting Epoch Timestamps:
+-- Getting the epoch timestamps:
 local now = os.time()
-print(now) --> 1715136403
-
--- Calculates epoch timestamps for specific datetimes:
 local today = os.time({ year = 2024, month = 05, day = 07, hour = 00 })
 local tomorrow = os.time({ year = 2024, month = 05, day = 08, hour = 00 })
+print(now)      --> 1715136403
 print(today)    --> 1715050800
 print(tomorrow) --> 1715137200
 
--- calculates the difference between two timestamps in seconds:
+-- Calculates the difference between two timestamps:
 local one_day = os.difftime(tomorrow, today)
-print(one_day) --> 86400.0 (seconds)
+print(one_day) --> 86400.0 (24*60*60 = 86400 seconds)
 
--- Formats a timestamp into a human-readable datetime string.
-local traditional_form = "%d/%m/%Y %X"
-print(os.date(traditional_form, now))           --> 07/05/2024 23:46:43
-print(os.date(traditional_form, now + one_day)) --> 08/05/2024 23:46:43
-print(os.date(traditional_form, today))         --> 07/05/2024 00:00:00
-print(os.date(traditional_form, tomorrow))      --> 08/05/2024 00:00:00
+-- Formats a timestamp into a datetime string:
+local datetime = "%d/%m/%Y %X"
+print(os.date(datetime, now))           --> 07/05/2024 23:46:43
+print(os.date(datetime, now + one_day)) --> 08/05/2024 23:46:43
+print(os.date(datetime, today))         --> 07/05/2024 00:00:00
+print(os.date(datetime, tomorrow))      --> 08/05/2024 00:00:00
 
--- Customizing date output formats:
-local customized_form = "%A, %d in %B in %Y"
-print(os.date(customized_form, today))    --> Tuesday, 07 in May in 2024
-print(os.date(customized_form, tomorrow)) --> Wednesday, 08 in May in 2024
+-- Formats a timestamp into a custom date string:
+local customdate = "%A, %d in %B in %Y"
+print(os.date(customdate, today))    --> Tuesday, 07 in May in 2024
+print(os.date(customdate, tomorrow)) --> Wednesday, 08 in May in 2024
 
--- Converts a timestamp into a table with datetime components:
+-- Extracting datetime components:
 local datetime = os.date("*t", now)
 print(datetime.year)  --> 2024
 print(datetime.month) --> 5
