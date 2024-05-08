686473506329330dc7e87938e9d0d83c7c8dc3af
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-08 16:26:07 -0300

    Create arithmetic.lua

---
+++ arithmetic.lua
@@ -0,0 +1,25 @@
+-- arithmetic.lua
+
+-- addition:
+print(3 + 4) --> 7
+
+-- subtraction:
+print(3 - 4) --> -1
+
+-- multiplication:
+print(3 * 4) --> 12
+
+-- exponentiation
+print(2 ^ 3) --> 8.0
+
+-- float division:
+print(7 / 3) --> 2.3333333333333
+
+-- floor division:
+print(7 // 3) --> 2
+
+-- modulo:
+print(7 % 3) --> 1
+
+-- unary minus:
+print(-1) --> -1
