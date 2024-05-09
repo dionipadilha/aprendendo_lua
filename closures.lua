98785f50e0536783a6f1152351ea03460bf639ad
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-08 21:33:12 -0300

    Update closures.lua

--- closures.lua
+++ closures.lua
@@ -1,5 +1,6 @@
 -- closures.lua
 
+-- Create functions that retain state between calls.
 local newCounter = function(n)
     local i = 0
     return function()
@@ -8,24 +9,25 @@ local newCounter = function(n)
     end
 end
 
+-- Creates a separate counter with its own environment:
 local c1 = newCounter(3)
 local c2 = newCounter(5)
 
-print(c1())   --> 1
-print(c1())   --> 2
-print(c2())   --> 1
+print(c1()) --> 1
+print(c1()) --> 2
+print(c2()) --> 1
 
-print(c1())   --> 3
-print(c2())   --> 2
+print(c1()) --> 3
+print(c2()) --> 2
 
-print(c1())   --> nil
-print(c2())   --> 3
+print(c1()) --> nil
+print(c2()) --> 3
 
-print(c1())   --> nil
-print(c2())   --> 4
+print(c1()) --> nil
+print(c2()) --> 4
 
-print(c1())   --> nil
-print(c2())   --> 5
+print(c1()) --> nil
+print(c2()) --> 5
 
-print(c1())   --> nil
-print(c2())   --> nil
+print(c1()) --> nil
+print(c2()) --> nil
