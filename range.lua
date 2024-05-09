156fab72c7df04920013aeab8fe7d654b9ca2ee8
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-09 08:11:26 -0300

    Update range.lua

--- range.lua
+++ range.lua
@@ -1,8 +1,6 @@
 -- range_reverse.lua
 
 ---------------------------------------------------------------
-
-
 local function range(start, stop, step)
   if not stop then
     stop = start
@@ -15,31 +13,30 @@ local function range(start, stop, step)
     return i <= stop and i or nil
   end
 end
-
-
 ---------------------------------------------------------------
 local t = {}
+table.print = function(self) print(table.concat(self, ", ")) end
 
 -- Starting from 1 up to the n:
 t = {}
 for n in range(3) do
   table.insert(t, n)
 end
-print(table.concat(t, ", ")) --> 1, 2, 3
+table.print(t) --> 1, 2, 3
 
 -- Increment between values:
 t = {}
 for n in range(2, 5) do
   table.insert(t, n)
 end
-print(table.concat(t, ", ")) --> 2, 3, 4, 5
+table.print(t) --> 2, 3, 4, 5
 
 -- Specifying the incrementation:
 t = {}
 for n in range(2, 6, 2) do
   table.insert(t, n)
 end
-print(table.concat(t, ", ")) --> 2, 4, 6
+table.print(t) --> 2, 4, 6
 
 --
 t = {}
@@ -47,11 +44,11 @@ local sequence = range(1, 10, 2)
 for n in sequence do
   table.insert(t, n)
 end
-print(table.concat(t, ", ")) --> 1, 3, 5, 7, 9
+table.print(t) --> 1, 3, 5, 7, 9
 
 -- Reverse range from 5 to 1
 t = {}
 for n in range(5, 1, -1) do
   table.insert(t, n)
 end
-print(table.concat(t, ", ")) --> 5, 4, 3, 2, 1
+table.print(t) --> 5, 4, 3, 2, 1
