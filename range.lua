debd5d402f1382d4131efa4bd82f44ad8adfb9f8
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-09 22:22:24 -0300

    Update range.lua

--- range.lua
+++ range.lua
@@ -1,54 +1,42 @@
 -- range.lua
 
----------------------------------------------------------------
+------------------------------------------------------------
+-- Generates a sequence similar to python's range:
+
 local function range(start, stop, step)
-  if not stop then
-    stop = start
-    start = 1
-  end
+  if not stop then stop, start = start, 1 end
   step = step or 1
   local i = start - step
   return function()
     i = i + step
+    if step < 0 then return i >= stop and i or nil end
     return i <= stop and i or nil
   end
 end
----------------------------------------------------------------
-local t = {}
-table.print = function(self) print(table.concat(self, ", ")) end
 
--- Starting from 1 up to the n:
-t = {}
-for n in range(3) do
-  table.insert(t, n)
-end
-table.print(t) --> 1, 2, 3
+------------------------------------------------------------
+-- Test cases:
 
--- Increment between values:
-t = {}
-for n in range(2, 5) do
-  table.insert(t, n)
+local function printCase(rangeGen)
+  local t = {}
+  for n in rangeGen do table.insert(t, n) end
+  return table.concat(t, ", ")
 end
-table.print(t) --> 2, 3, 4, 5
 
--- Specifying the incrementation:
-t = {}
-for n in range(2, 6, 2) do
-  table.insert(t, n)
-end
-table.print(t) --> 2, 4, 6
+local oddNumbers = range(1, 10, 2)
 
---
-t = {}
-local sequence = range(1, 10, 2)
-for n in sequence do
-  table.insert(t, n)
-end
-table.print(t) --> 1, 3, 5, 7, 9
+local cases = {
+  printCase(range(3)) == "1, 2, 3",
+  printCase(range(2, 5)) == "2, 3, 4, 5",
+  printCase(range(2, 6, 2)) == "2, 4, 6",
+  printCase(range(-5, -1, 2)) == "-5, -3, -1",
+  printCase(range(5, 1, -2)) == "5, 3, 1",
+  printCase(oddNumbers) == "1, 3, 5, 7, 9"
+}
 
--- Reverse range from 5 to 1
-t = {}
-for n in range(5, 1, -1) do
-  table.insert(t, n)
+for n, case in ipairs(cases) do
+  assert(case, "Error case: " .. n)
 end
-table.print(t) --> 5, 4, 3, 2, 1
+
+------------------------------------------------------------
+return range
