5aef2c3c7610e7b13b38107033268ab1a8f6ba65
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-13 11:11:05 -0300

    Update range.lua

--- range.lua
+++ range.lua
@@ -1,41 +1,35 @@
 -- range.lua
 
-------------------------------------------------------------
--- Generates a sequence similar to python's range:
+-- Generates a sequence of numbers.
 
-local function range(start, stop, step)
-  if not stop then stop, start = start, 1 end
+local function inputHandler(start, stop, step)
+  -- If only one arg is provided, it sets the stop:
+  if not stop then
+    stop, start = start, 1
+  end
+  -- Default step is 1 if not provided:
   step = step or 1
+  return start, stop, step
+end
+
+local function getIterator(start, stop, step)
   local i = start - step
   return function()
     i = i + step
-    if step < 0 then return i >= stop and i or nil end
-    return i <= stop and i or nil
+    -- Increments or decrements i based on the step value:
+    if step < 0 then
+      -- generates a decreasing sequence:
+      return i >= stop and i or nil
+    else
+      -- generates a increasing sequence:
+      return i <= stop and i or nil
+    end
   end
 end
 
-------------------------------------------------------------
--- Test cases:
-
-local function printRange(rangeGen)
-  local t = {}
-  for n in rangeGen do table.insert(t, n) end
-  local result = table.concat(elements, ", ")
-  return result
+local function range(start, stop, step)
+  start, stop, step = inputHandler(start, stop, step)
+  return getIterator(start, stop, step)
 end
 
-local oddNumbers = range(1, 10, 2)
-
-local cases = {
-  printRange(range(3)) == "1, 2, 3",
-  printRange(range(2, 5)) == "2, 3, 4, 5",
-  printRange(range(2, 6, 2)) == "2, 4, 6",
-  printRange(range(-5, -1, 2)) == "-5, -3, -1",
-  printRange(range(5, 1, -2)) == "5, 3, 1",
-  printRange(oddNumbers) == "1, 3, 5, 7, 9"
-}
-
-for n, case in ipairs(cases) do
-  assert(case, "Error case: " .. n)
-end
-------------------------------------------------------------
+return range
