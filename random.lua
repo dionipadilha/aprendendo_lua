dbfea368f24f3812262600f715031e609719b370
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-16 16:11:43 -0300

    Update random.lua

--- random.lua
+++ random.lua
@@ -16,10 +16,23 @@ math.randomseed(1715878000)
 -- two arguments: combine args into a single 128-bit seed.
 math.randomseed(1715878877, 15471192)
 
--- avoid using math.randomseed without arguments:
+--------------------------------------------------------------------------------
+-- enhanced math.randomseed:
+
+-- avoid using math.randomseed without arguments
 -- using the same seed will always produce the same sequence of random numbers.
+
 -- for better randomness try:
 print(math.randomseed(os.time())) --> 1715880897	0
+
+-- using multiple entropy sources
+
+local sources = {
+  tostring(os.time()):reverse():sub(1, 6),
+  tostring(os.clock()):reverse():sub(1, 6)
+}
+local seed = tonumber(table.concat(sources))
+print(math.randomseed(seed)) --> 485588220	0
 --------------------------------------------------------------------------------
 -- math.random:
 
@@ -54,8 +67,7 @@ print(randomChoice(colors)) --> green
 -- example #2: generate a random alphanumeric string of a specified length
 
 
-local function randomString(n)
-  local charset = "abcdefghijklmnopqrstuvwxyz0123456789"
+local function randomPassword(charset, n)
   local result = {}
   for i = 1, n do
     local rand = math.random(1, #charset)
@@ -65,23 +77,27 @@ local function randomString(n)
 end
 
 math.randomseed(os.time())
-print(randomString(8))  --> ap8dh2ds
-print(randomString(10)) --> md27chy7b0
+local charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+print(randomPassword(charset, 8))  --> AHWWxNVo
+print(randomPassword(charset, 10)) --> C8MrMpBG1g
 
 --------------------------------------------------------------------------------
--- example #3: random time generator over a period of one day
+-- example #3: random time generator with customizable range
 
-
-local function randomTimeGenerator()
-  local now = os.time()
-  local day = 86400              -- 24h * 60 min * 60s = 86400
+local function randomTimeGenerator(min, max)
   local datetime = "%d/%m/%Y %X" -- dd/mm/yyyy hh:mm:ss
-  local rand = math.random(now - day, now + day)
+  local rand = math.random(min, max)
   return (os.date(datetime, rand))
 end
 
-math.randomseed(os.time())
-print(randomTimeGenerator()) --> 16/05/2024 19:17:51
-print(randomTimeGenerator()) --> 16/05/2024 05:04:51
+local now = os.time()
+local day = 86400 -- 24 * 60 * 60s = 86400s
+local min = now - day
+local max = now + day
+
+math.randomseed(now)
+
+print(randomTimeGenerator(min, max)) --> 16/05/2024 19:17:51
+print(randomTimeGenerator(min, max)) --> 16/05/2024 05:04:51
 
 --------------------------------------------------------------------------------
