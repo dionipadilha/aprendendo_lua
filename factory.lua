9377c9f94fd0a33d0d8a2d199ab6ca3ca55e0c7b
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-20 14:32:26 -0300

    Update factory.lua

--- factory.lua
+++ factory.lua
@@ -1,5 +1,7 @@
 -- factory.lua
 
+-- building complex objects with interchangeable parts.
+
 --------------------------------------------------------------------------------
 -- #1.Factory:
 
@@ -19,19 +21,20 @@ function AbstractChassis:hold()
   error("This method should be overridden")
 end
 
--- Chassis Object:
+-- Chassis Object #1:
 local chassis_1 = AbstractChassis:new {}
 function chassis_1:hold()
   return "Chassis_1 ok"
 end
 
+-- Chassis Object #2:
 local chassis_2 = AbstractChassis:new {}
 function chassis_2:hold()
   return "Chassis_2 ok"
 end
 
 --------------------------------------------------------------------------------
--- #3. Engines:
+-- #3. Engine:
 
 -- Engine Factory:
 local AbstractEngine = Factory:new {}
@@ -39,12 +42,13 @@ function AbstractEngine:start()
   error("This method should be overridden")
 end
 
--- Engine Object:
+-- Engine Object #1:
 local engine_1 = AbstractEngine:new {}
 function engine_1:start()
   return "Engine_1 is roaring"
 end
 
+-- Engine Object #2:
 local engine_2 = AbstractEngine:new {}
 function engine_2:start()
   return "Engine_2 is roaring"
@@ -59,19 +63,20 @@ function AbstractTransmission:shift()
   error("This method should be overridden")
 end
 
--- Transmission Object:
+-- Transmission Object #1:
 local transmission_1 = AbstractTransmission:new {}
 function transmission_1:shift()
   return "Transmission_1 is shifting"
 end
 
+-- Transmission Object #2:
 local transmission_2 = AbstractTransmission:new {}
 function transmission_2:shift()
   return "Transmission_2 is shifting"
 end
 
 --------------------------------------------------------------------------------
--- #5. Wheels:
+-- #5. Wheel:
 
 -- Wheel Factory:
 local AbstractWheel = Factory:new {}
@@ -79,12 +84,13 @@ function AbstractWheel:spin()
   error("This method should be overridden")
 end
 
--- Wheel Object:
+-- Wheel Object #1:
 local wheel_1 = AbstractWheel:new {}
 function wheel_1:spin()
   return "Wheel_1 is spinning"
 end
 
+-- Wheel Object #2:
 local wheel_2 = AbstractWheel:new {}
 function wheel_2:spin()
   return "Wheel_2 is spinning"
@@ -96,37 +102,59 @@ end
 -- Car Factory:
 local AbstractCar = Factory:new {}
 
-function AbstractCar:drive()
-  assert(self.chassis and self.engine and self.transmission and self.wheel)
-  self.chassis:hold()
-  self.engine:start()
-  self.transmission:shift()
-  self.wheel:spin()
-  print("Car is driving: " .. self.id)
+function AbstractCar:check()
+  error("This method should be overridden")
 end
 
--- Car Object:
-local car_1 = AbstractCar:new {
-  id = "ana",
-  chassis = chassis_1:new {},
-  engine = engine_2:new {},
-  transmission = transmission_1:new {},
-  wheel = wheel_2:new {}
-}
+function AbstractCar:start()
+  print("self.check: ", pcall(self.check, self))
+end
 
-local car_2 = AbstractCar:new {
-  id = "bob",
-  chassis = chassis_2:new {},
+function AbstractCar:drive(pilot)
+  print("Car is driving: " .. pilot)
+end
+
+--------------------------------------------------------------------------------
+-- Car Object: #1
+
+local car_1 = AbstractCar:new {
+  chassis = chassis_1:new {},
   engine = engine_1:new {},
-  transmission = transmission_2:new {},
+  transmission = transmission_1:new {},
   wheel = wheel_1:new {}
 }
 
+function car_1:check()
+  assert(self.chassis and self.engine and self.transmission and self.wheel)
+  assert(self.chassis:hold(), "Chassis_1 ok")
+  assert(self.engine:start(), "Engine_1 is roaring")
+  assert(self.transmission:shift(), "Transmission_1 is shifting")
+  assert(self.wheel:spin(), "Wheel_1 is spinning")
+end
+
 --------------------------------------------------------------------------------
--- #7. User:
+-- Car Object: #2
 
-print(car_2.engine.start()) --> Engine_2 is roaring
-car_1:drive()               --> Car is driving:ana
+local car_2 = AbstractCar:new {
+  chassis = chassis_2:new {},
+  engine = engine_2:new {},
+  transmission = transmission_2:new {},
+  wheel = wheel_2:new {}
+}
 
-print(car_2.engine.start()) --> Engine_1 is roaring
-car_2:drive()               --> Car is driving: bob
+function car_2:check()
+  assert(self.chassis and self.engine and self.transmission and self.wheel)
+  assert(self.chassis:hold(), "Chassis_2 ok")
+  assert(self.engine:start(), "Engine_2 is roaring")
+  assert(self.transmission:shift(), "Transmission_2 is shifting")
+  assert(self.wheel:spin(), "Wheel_2 is spinning")
+end
+
+--------------------------------------------------------------------------------
+-- #7. Users:
+
+car_1:start()      --> self.check: 	true
+car_1:drive("ana") --> Car is driving: ana
+
+car_2:start()      --> self.check: 	true
+car_2:drive("bob") --> Car is driving: bob
