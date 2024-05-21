dfb3770135a8f0552dc3f1187bfe121b916d2949
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-21 09:37:37 -0300

    Update file_handling_basic.lua

--- file_handling_basic.lua
+++ file_handling_basic.lua
@@ -1,6 +1,6 @@
 -- file_handling_basic.lua
 
--- Approach #1: Basic File Handling
+-- Approach #1:
 
 -- Writing to a file
 local writeFile = io.open("test.txt", "w")
@@ -26,3 +26,15 @@ if content then
 else
     print("No content read from file.")
 end
+
+-- Approach #2:
+
+-- create a file if not exists:
+local file = assert(io.open("demofile.txt", "w"))
+file:write("toast!")
+assert(file:close())
+
+-- open a file for reading:
+file = assert(io.open("demofile.txt"))
+print(file:read()) --> toast!
+assert(file:close())
