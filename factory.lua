5ff2cda8754c641e202a27a465853d9a20476ca9
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-20 21:16:43 -0300

    Update factory.lua

--- factory.lua
+++ factory.lua
@@ -6,10 +6,12 @@
 -- #1.Factory:
 
 local Factory = {}
+
 function Factory:new(object)
   object = object or {}
+  setmetatable(object, self)
   self.__index = self
-  return setmetatable(object, self)
+  return object
 end
 
 --------------------------------------------------------------------------------
@@ -17,18 +19,21 @@ end
 
 -- Chassis Factory:
 local FactoryChassis = Factory:new {}
+
 function FactoryChassis:hold()
   error("This method should be overridden")
 end
 
 -- Chassis Type #1:
 local chassisType_1 = FactoryChassis:new {}
+
 function chassisType_1:hold()
   return "ChassisType_1 ok"
 end
 
 -- Chassis Type #2:
 local chassisType_2 = FactoryChassis:new {}
+
 function chassisType_2:hold()
   return "ChassisType_2 ok"
 end
@@ -38,18 +43,21 @@ end
 
 -- Engine Factory:
 local FactoryEngine = Factory:new {}
+
 function FactoryEngine:start()
   error("This method should be overridden")
 end
 
 -- Engine Type #1:
 local engineType_1 = FactoryEngine:new {}
+
 function engineType_1:start()
   return "EngineType_1 is roaring"
 end
 
 -- Engine Type #2:
 local engineType_2 = FactoryEngine:new {}
+
 function engineType_2:start()
   return "EngineType_2 is roaring"
 end
@@ -59,18 +67,21 @@ end
 
 -- Transmission Factory:
 local FactoryTransmission = Factory:new {}
+
 function FactoryTransmission:shift()
   error("This method should be overridden")
 end
 
 -- Transmission Type #1:
 local transmissionType_1 = FactoryTransmission:new {}
+
 function transmissionType_1:shift()
   return "TransmissionType_1 is shifting"
 end
 
 -- Transmission Type #2:
 local transmissionType_2 = FactoryTransmission:new {}
+
 function transmissionType_2:shift()
   return "TransmissionType_2 is shifting"
 end
@@ -80,18 +91,21 @@ end
 
 -- Wheel Factory:
 local FactoryWheel = Factory:new {}
+
 function FactoryWheel:spin()
   error("This method should be overridden")
 end
 
 -- Wheel Type #1:
 local wheelType_1 = FactoryWheel:new {}
+
 function wheelType_1:spin()
   return "WheelType_1 is spinning"
 end
 
 -- Wheel Type #2:
 local wheelType_2 = FactoryWheel:new {}
+
 function wheelType_2:spin()
   return "WheelType_2 is spinning"
 end
@@ -118,14 +132,14 @@ end
 --------------------------------------------------------------------------------
 -- Car Type #1
 
-local carType_1 = FactoryCar:new {
+local Ford = FactoryCar:new {
   chassis = chassisType_1:new {},
   engine = engineType_1:new {},
   transmission = transmissionType_1:new {},
   wheel = wheelType_1:new {}
 }
 
-function carType_1:check()
+function Ford:check()
   assert(self.chassis and self.engine and self.transmission and self.wheel)
   assert(self.chassis:hold() == "ChassisType_1 ok")
   assert(self.engine:start() == "EngineType_1 is roaring")
@@ -137,14 +151,14 @@ end
 --------------------------------------------------------------------------------
 -- Car Type #2
 
-local carType_2 = FactoryCar:new {
+local Tesla = FactoryCar:new {
   chassis = chassisType_2:new {},
   engine = engineType_2:new {},
   transmission = transmissionType_2:new {},
   wheel = wheelType_2:new {}
 }
 
-function carType_2:check()
+function Tesla:check()
   assert(self.chassis and self.engine and self.transmission and self.wheel)
   assert(self.chassis:hold() == "ChassisType_2 ok")
   assert(self.engine:start() == "EngineType_2 is roaring")
@@ -156,8 +170,8 @@ end
 --------------------------------------------------------------------------------
 -- #7. Testing the Cars:
 
-carType_1:start()      --> self.check: 	true	All components are functional
-carType_1:drive("ana") --> Car is driving: ana
+Ford:start()       --> self.check: 	true	All components are functional
+Ford:drive("ana")  --> Car is driving: ana
 
-carType_2:start()      --> self.check: 	true	All components are functional
-carType_2:drive("bob") --> Car is driving: bob
+Tesla:start()      --> self.check: 	true	All components are functional
+Tesla:drive("bob") --> Car is driving: bob
