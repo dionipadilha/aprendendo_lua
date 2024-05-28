1d2a062745b9a24aaead6ca8871d92a7d0216338
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-28 13:32:07 -0300

    Update anonymous_functios.lua

--- anonymous_functios.lua
+++ anonymous_functios.lua
@@ -2,17 +2,17 @@
 
 -- Anonymous function execution:
 local y = (function(x) return 2 * x end)(3)
-print(y) --> 6
+assert(y == 6)
 
 -- Variable with anonymous functions:
 local doubler = function(x) return 2 * x end
-print(doubler(3)) --> 6
-print(doubler(5)) --> 10
+assert(doubler(3) == 6)
+assert(doubler(5) == 10)
 
 -- Anonymous function with multiple arguments:
 local plus = function(a, b) return a + b end
-print(plus(2, 3)) --> 5
-print(plus(5, 7)) --> 12
+assert(plus(2, 3) == 5)
+assert(plus(5, 7) == 12)
 
 -- Anonymous function and First-class functions:
 local function multiplyBy(x)
@@ -22,15 +22,15 @@ local function multiplyBy(x)
 end
 local timesTwo = multiplyBy(2)
 local timesFive = multiplyBy(5)
-print(timesTwo(3))  --> 6
-print(timesFive(3)) --> 15
+assert(timesTwo(3) == 6)
+assert(timesFive(3) == 15)
 
 -- Anonymous function and  Higher-order function:
 local function evaluate(f, x)
-  return print(f(x))
+  return f(x)
 end
-evaluate(function(x) return x ^ (1 / 2) end, 9) --> 3.0
-evaluate(function(x) return x ^ 2 end, 3)       --> 9.0
+assert(evaluate(function(x) return x ^ (1 / 2) end, 9) == 3.0)
+assert(evaluate(function(x) return x ^ 2 end, 3) == 9.0)
 
 -- Anonymous function and Closures:
 local function newCounter()
@@ -41,6 +41,6 @@ local function newCounter()
   end
 end
 local count = newCounter()
-print(count()) --> 1
-print(count()) --> 2
-print(count()) --> 3
+assert(count() == 1)
+assert(count() == 2)
+assert(count() == 3)
