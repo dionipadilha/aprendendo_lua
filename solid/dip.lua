7d9e6be91e26517062accf81221faef9d05ac5e9
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-05-29 14:07:18 -0300

    Update dip.lua

--- solid/dip.lua
+++ solid/dip.lua
@@ -3,36 +3,52 @@
 -- Dependency Inversion Principle:
 -- Depend on abstractions, not on concretions.
 
--- Abstraction
-local Database = {}
+-- #1. Abstraction Interface:
+local Logger = {}
 
-function Database:connect()
+function Logger:new(instance)
+  self.__index = self
+  instance = instance or {}
+  return setmetatable(instance, self)
+end
+
+function Logger:log(message)
   error("This method should be overridden")
 end
 
--- Low-level module
-local MySQLDatabase = Database:new()
-
-function MySQLDatabase:connect()
-  print("Connecting to MySQL database")
+-- #2. Concrete Implementations:
+local FileLogger = Logger:new {}
+function FileLogger:log(message)
+  local file, err = io.open(self.filename, "a")
+  assert(file, err)
+  file:write(message .. "\n")
+  file:close()
 end
 
--- High-level module
-local PasswordReminder = {}
+local ConsoleLogger = Logger:new {}
+function ConsoleLogger:log(message)
+  print(message)
+end
 
-function PasswordReminder:new(database)
-  local o = {}
-  setmetatable(o, self)
+-- High-Level Module:
+Application = {}
+
+function Application:new(instance)
+  assert(instance.logger.log, "required: logger.log")
   self.__index = self
-  self.database = database
-  return o
+  return setmetatable(instance, self)
 end
 
-function PasswordReminder:connect()
-  self.database:connect()
+function Application:doWork()
+  self.logger:log("working ...")
 end
 
--- Test
-local mySQL = MySQLDatabase:new()
-local reminder = PasswordReminder:new(mySQL)
-reminder:connect()
+-- Usage Example:
+local fileLogger = FileLogger:new { filename = "log.txt" }
+local consoleLogger = ConsoleLogger:new {}
+
+local app1 = Application:new { logger = fileLogger }
+local app2 = Application:new { logger = consoleLogger }
+
+app1:doWork() -- Logs to a file
+app2:doWork() -- Logs to the console
