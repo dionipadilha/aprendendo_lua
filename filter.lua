dc44ff128f2e6609ebfb155e226ddb33b946de40
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-06-11 17:25:23 -0300

    Update filter.lua

--- filter.lua
+++ filter.lua
@@ -1,21 +1,11 @@
 -- filter.lua
 
--- Function to check if a number is even
-local function isEven(n)
-  return n % 2 == 0
-end
-
--- Function to check if a number is odd
-local function isOdd(n)
-  return n % 2 ~= 0
-end
-
--- Function to filter a list based on a given criterion
-local function filter(list, criterion)
+-- Function to filter a list based on a given criterion:
+local function filter(list, crit)
   --
   local expected = {
     type(list) == "table",
-    type(criterion) == "function",
+    type(crit) == "function",
   }
   assert(expected[1], "Expected a table as the first argument.")
   assert(expected[2], "Expected a function as the second argument.")
@@ -23,13 +13,22 @@ local function filter(list, criterion)
   --
   local filteredList = {}
   for _, element in ipairs(list) do
-    if criterion(element) then
+    if crit(element) then
       table.insert(filteredList, element)
     end
   end
 
   --
   return filteredList
+end
+
+-- Generic filters:
+local function isEven(n)
+  return n % 2 == 0
+end
+
+local function isOdd(n)
+  return n % 2 ~= 0
 end
 
 -- List to be filtered
