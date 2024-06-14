a12262dfe4e65ab47659e5587bbbbd8099a789ff
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-06-14 12:25:37 -0300

    Update switch.lua

--- switch.lua
+++ switch.lua
@@ -1,17 +1,44 @@
-local function switch(case)
-  -- Define a table to hold all possible cases
-  local cases = {
-    x = function() print("This is case 1") end,
-    y = function() print("This is case 2") end,
-    z = function() print("This is case 3") end,
-    -- Add more cases as needed
-    default = function() print("This is the default case") end
-  }
-  return (cases[case] or cases["default"])()
+--------------------------------------------------------------------------------
+-- switch Function
+
+local function switch(cases, value)
+  assert(type(cases) == "table", "Invalid table cases")
+  assert(cases["default"], "Provide the default case")
+  assert(
+    type(value) == "string" or type(value) == "number",
+    "value must be a string or number"
+  )
+  local action = cases[value] or cases["default"]
+  assert(type(action) == "function", "action must be a function")
+  return action()
 end
 
-local userInputs = { "x", "z", 0, "" }
+--------------------------------------------------------------------------------
+-- Example Usage #1
 
-for _, value in ipairs(userInputs) do
-  switch(value)
-end
+local students = {
+  ana = function() print("This is case 1") end,
+  bob = function() print("This is case 2") end,
+  charlie = function() print("This is case 3") end,
+  -- Add more cases as needed
+  default = function() print("This is the default case") end
+}
+
+switch(students, "ana")  --> This is case 1
+switch(students, "bob")  --> This is case 2
+switch(students, "duda") --> This is the default case
+
+--------------------------------------------------------------------------------
+-- Example Usage #2
+
+local friends = {
+  jhon = function() print("This is case 1") end,
+  jane = function() print("This is case 2") end,
+  jack = function() print("This is case 3") end,
+  -- Add more cases as needed
+  default = function() print("This is the default case") end
+}
+
+switch(friends, "jack") --> This is case 3
+switch(friends, "bob")  --> This is the default case
+--------------------------------------------------------------------------------
