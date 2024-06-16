bc8fabdd5c9c5b3d780a2c1f658876bee27c9043
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-06-15 21:33:25 -0300

    Update stopwatch.lua

--- stopwatch.lua
+++ stopwatch.lua
@@ -1,34 +1,69 @@
--- Define a stopwatch:
-local stopwatch = {}
+-- Stopwatch class definition
+local Stopwatch = {}
 
-function stopwatch.start(self)
+function Stopwatch:new(object)
+  object = object or {}
+  setmetatable(object, self)
+  self.__index = self
+  return object
+end
+
+function Stopwatch:start()
   self.initial = os.clock()
   return self.initial
 end
 
-function stopwatch.stop(self)
+function Stopwatch:stop()
   self.final = os.clock()
   return self.final
 end
 
-function stopwatch.diff(self)
+function Stopwatch:_diff()
   return self.final - self.initial
 end
 
--- Define a function:
-local function delay(n)
-  assert(n > 0)
-  local start_time = os.clock()
-  while os.clock() - start_time < n do
-    -- nothing
-  end
+function Stopwatch:log()
+  local elapsedTime = self:_diff()
+  return string.format("elapsed time: %.5f seconds.", elapsedTime)
 end
 
--- Create an instance of the stopwatch:
-local myStopwatch = setmetatable({}, { __index = stopwatch })
+function Stopwatch:reset()
+  self.initial = 0
+  self.final = 0
+  return true
+end
 
--- Stopwatch the recursive factorial function:
-print("start: ", myStopwatch:start())
-delay(3)
-print("stop: ", myStopwatch:stop())
-print(string.format("elapsed time: %.5f seconds.", myStopwatch:diff()))
+-- Example usage:
+
+local function main()
+  -- Delay utility function
+  local function busyDelay(n)
+    assert(type(n) == "number" and n > 0, "Delay duration must be positive.")
+    local start_time = os.clock()
+    while os.clock() - start_time < n do
+      -- wait
+    end
+  end
+
+  -- Create an instance of the stopwatch:
+  local myStopwatch = Stopwatch:new {
+    initial = 0,
+    final = 0
+  }
+
+  -- Use the stopwatch to time the delay function
+  local start = myStopwatch:start()
+  busyDelay(3)
+  local stop = myStopwatch:stop()
+
+  -- Print results
+  print("start: ", start)
+  print("stop: ", stop)
+  print(myStopwatch:log())
+
+  -- Reset the stopwatch
+  myStopwatch:reset()
+  assert(myStopwatch:log() == "elapsed time: 0.00000 seconds.")
+end
+
+main()
