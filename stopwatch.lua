cae8881dfb1d3c2044df8c38b90ed57cc67da39a
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-06-17 15:31:04 -0300

    Update stopwatch.lua

--- stopwatch.lua
+++ stopwatch.lua
@@ -1,71 +1,92 @@
--- Stopwatch class definition
-local Stopwatch = { initial = nil, final = nil }
+--------------------------------------------------------------------------------
+-- Stopwatch Implementation:
 
-function Stopwatch:new(object)
-  object = object or {}
-  setmetatable(object, self)
+local IStopwatch = {}
+
+function IStopwatch:new(contract)
+  contract = contract or {}
+  setmetatable(contract, self)
   self.__index = self
-  return object
+  return contract
 end
 
+function IStopwatch:start() error("Implementation by concrete class") end
+
+function IStopwatch:stop() error("Implementation by concrete class") end
+
+function IStopwatch:log() error("Implementation by concrete class") end
+
+function IStopwatch:reset() error("Implementation by concrete class") end
+
+--------------------------------------------------------------------------------
+-- Concrete Stopwatch implementation
+local Stopwatch = IStopwatch:new { startTime = 0, stopTime = 0, running = false }
+
 function Stopwatch:start()
-  self.initial = os.clock()
-  return self.initial
+  self.running = true
+  self.startTime = os.clock()
 end
 
 function Stopwatch:stop()
-  assert(self.initial ~= 0, "Stopwatch has not been started.")
-  self.final = os.clock()
-  return self.final
-end
-
-function Stopwatch:_diff()
-  assert(
-    self.initial and self.final,
-    "Stopwatch has not been properly started or stopped."
-  )
-  return self.final - self.initial
+  self.stopTime = os.clock()
+  self.running = false
 end
 
 function Stopwatch:log()
-  local elapsedTime = self:_diff()
-  return string.format("elapsed time: %.6f seconds.", elapsedTime)
+  local elapsedTime = 0
+  if self.running then
+    elapsedTime = os.clock() - self.startTime
+    return "Stopwatch is running. Elapsed time: " .. elapsedTime .. " seconds."
+  else
+    elapsedTime = self.stopTime - self.startTime
+    return "Stopwatch stopped. Elapsed time: " .. elapsedTime .. " seconds."
+  end
 end
 
 function Stopwatch:reset()
-  self.initial = 0
-  self.final = 0
-  return true
+  self.startTime = 0
+  self.stopTime = 0
+  self.running = false
 end
 
--- Example usage:
+--------------------------------------------------------------------------------
+-- Stopwatch Testing:
 
-local function main()
-  -- Delay utility function
-  local function busyDelay(n)
-    assert(type(n) == "number" and n > 0, "Delay duration must be positive.")
-    local start_time = os.clock()
-    while os.clock() - start_time < n do
-      -- wait
-    end
+-- Define a delay function to simulate a time-consuming task
+local function busyDelay(seconds)
+  assert(type(seconds) == "number" and seconds > 0, "Delay duration must be positive.")
+  local start_time = os.clock()
+  while os.clock() - start_time < seconds do
+    -- busy wait
   end
+end
 
-  -- Create an instance of the stopwatch:
-  local myStopwatch = Stopwatch:new { initial = 0, final = 0 }
+-- Usage with dependency injection
+local function test(stopwatch)
+  stopwatch:start()
 
-  -- Use the stopwatch to time the delay function
-  local start = myStopwatch:start()
-  busyDelay(3)
-  local stop = myStopwatch:stop()
+  -- Perform some timed operations
+  print("#1 ...")
+  busyDelay(1)
+  print("#2 ...")
+  busyDelay(1)
+  print(stopwatch:log()) --> Stopwatch running. Elapsed time: x seconds.
+  print("#3 ...")
+  busyDelay(1)
+  stopwatch:stop()
 
-  -- Print results
-  print("start: ", start)
-  print("stop: ", stop)
-  print(myStopwatch:log())
+  -- Log stopwatch results
+  print(stopwatch:log()) --> Stopwatch stopped. Elapsed time: y seconds.
 
   -- Reset the stopwatch
-  myStopwatch:reset()
-  assert(myStopwatch:log() == "elapsed time: 0.000000 seconds.")
+  stopwatch:reset()
+
+  -- Log stopwatch results
+  assert(stopwatch:log() == "Stopwatch stopped. Elapsed time: 0 seconds.")
 end
 
-main()
+-- Usage
+local myStopwatch = Stopwatch:new()
+test(myStopwatch)
+
+--------------------------------------------------------------------------------
