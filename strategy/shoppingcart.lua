753d53b953fe1d486f4a11e2e763fc3e47dcfc7d
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-06-17 10:08:32 -0300

    Create shoppingcart.lua

---
+++ strategy/shoppingcart.lua
@@ -0,0 +1,29 @@
+local ShoppingCart = {
+  items = {},
+  paymentStrategy = nil
+}
+
+function ShoppingCart:new(object)
+  object = object or {}
+  setmetatable(object, self)
+  self.__index = self
+  return object
+end
+
+function ShoppingCart:addItem(item)
+  table.insert(self.items, item)
+end
+
+function ShoppingCart:setPayment(strategy)
+  self.paymentStrategy = strategy
+end
+
+function ShoppingCart:checkout()
+  local totalAmount = 0
+  for _, item in ipairs(self.items) do
+    totalAmount = totalAmount + item.price
+  end
+  return self.paymentStrategy:pay(totalAmount)
+end
+
+return ShoppingCart
