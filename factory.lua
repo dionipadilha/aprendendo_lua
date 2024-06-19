cb07691a70b03a8edeed898ce5794609b4c80c67
Dioni Padilha <137119331+dionipadilha@users.noreply.github.com>
2024-06-18 21:21:07 -0300

    Update factory.lua

--- factory.lua
+++ factory.lua
@@ -3,172 +3,159 @@
 -- building complex objects with interchangeable parts.
 
 --------------------------------------------------------------------------------
--- #1.Factory:
+-- Factory Base Class:
 
 local Factory = {}
 
-function Factory:new(object)
-  object = object or {}
-  setmetatable(object, self)
+function Factory:new(instance)
+  instance = instance or {}
+  setmetatable(instance, self)
   self.__index = self
-  return object
+  return instance
 end
 
 --------------------------------------------------------------------------------
--- #2. Chassis:
+-- Chassis Factory and Types:
 
--- Chassis Factory:
-local FactoryChassis = Factory:new {}
+local ChassisFactory = Factory:new {}
 
-function FactoryChassis:hold()
+function ChassisFactory:hold()
   error("This method should be overridden")
 end
 
--- Chassis Type #1:
-local chassisType_1 = FactoryChassis:new {}
+local chassisType1 = ChassisFactory:new {}
 
-function chassisType_1:hold()
-  return "ChassisType_1 ok"
+function chassisType1:hold()
+  return "ChassisType1 ok"
 end
 
--- Chassis Type #2:
-local chassisType_2 = FactoryChassis:new {}
+local chassisType2 = ChassisFactory:new {}
 
-function chassisType_2:hold()
-  return "ChassisType_2 ok"
+function chassisType2:hold()
+  return "ChassisType2 ok"
 end
 
 --------------------------------------------------------------------------------
--- #3. Engine:
+-- Engine Factory and Types:
 
--- Engine Factory:
-local FactoryEngine = Factory:new {}
+local EngineFactory = Factory:new {}
 
-function FactoryEngine:start()
+function EngineFactory:start()
   error("This method should be overridden")
 end
 
--- Engine Type #1:
-local engineType_1 = FactoryEngine:new {}
+local engineType1 = EngineFactory:new {}
 
-function engineType_1:start()
-  return "EngineType_1 is roaring"
+function engineType1:start()
+  return "EngineType1 is roaring"
 end
 
--- Engine Type #2:
-local engineType_2 = FactoryEngine:new {}
+local engineType2 = EngineFactory:new {}
 
-function engineType_2:start()
-  return "EngineType_2 is roaring"
+function engineType2:start()
+  return "EngineType2 is roaring"
 end
 
 --------------------------------------------------------------------------------
--- #4. Transmission:
+-- Transmission Factory and Types:
 
--- Transmission Factory:
-local FactoryTransmission = Factory:new {}
+local TransmissionFactory = Factory:new {}
 
-function FactoryTransmission:shift()
+function TransmissionFactory:shift()
   error("This method should be overridden")
 end
 
--- Transmission Type #1:
-local transmissionType_1 = FactoryTransmission:new {}
+local transmissionType1 = TransmissionFactory:new {}
 
-function transmissionType_1:shift()
-  return "TransmissionType_1 is shifting"
+function transmissionType1:shift()
+  return "TransmissionType1 is shifting"
 end
 
--- Transmission Type #2:
-local transmissionType_2 = FactoryTransmission:new {}
+local transmissionType2 = TransmissionFactory:new {}
 
-function transmissionType_2:shift()
-  return "TransmissionType_2 is shifting"
+function transmissionType2:shift()
+  return "TransmissionType2 is shifting"
 end
 
 --------------------------------------------------------------------------------
--- #5. Wheel:
+-- Wheel Factory and Types:
 
--- Wheel Factory:
-local FactoryWheel = Factory:new {}
+local WheelFactory = Factory:new {}
 
-function FactoryWheel:spin()
+function WheelFactory:spin()
   error("This method should be overridden")
 end
 
--- Wheel Type #1:
-local wheelType_1 = FactoryWheel:new {}
+local wheelType1 = WheelFactory:new {}
 
-function wheelType_1:spin()
-  return "WheelType_1 is spinning"
+function wheelType1:spin()
+  return "WheelType1 is spinning"
 end
 
--- Wheel Type #2:
-local wheelType_2 = FactoryWheel:new {}
+local wheelType2 = WheelFactory:new {}
 
-function wheelType_2:spin()
-  return "WheelType_2 is spinning"
+function wheelType2:spin()
+  return "WheelType2 is spinning"
 end
 
 --------------------------------------------------------------------------------
--- #6. Car Assembly:
+-- Car Assembly and Types:
 
--- Car Factory:
-local FactoryCar = Factory:new {}
+local CarFactory = Factory:new {}
 
-function FactoryCar:check()
+function CarFactory:check()
   error("This method should be overridden")
 end
 
-function FactoryCar:start()
+function CarFactory:start()
   local status, result = pcall(self.check, self)
   print("self.check: ", status, result)
 end
 
-function FactoryCar:drive(pilot)
-  print("Car is driving by: " .. pilot)
+function CarFactory:drive(driver)
+  print("Car is driving by: " .. driver)
 end
 
 --------------------------------------------------------------------------------
--- Car Type #1
+-- Ford Car Type:
 
-local Ford = FactoryCar:new {
-  chassis = chassisType_1:new {},
-  engine = engineType_1:new {},
-  transmission = transmissionType_1:new {},
-  wheel = wheelType_1:new {}
+local Ford = CarFactory:new {
+  chassis = chassisType1:new {},
+  engine = engineType1:new {},
+  transmission = transmissionType1:new {},
+  wheel = wheelType1:new {}
 }
 
 function Ford:check()
   assert(self.chassis and self.engine and self.transmission and self.wheel)
-  assert(self.chassis:hold() == "ChassisType_1 ok")
-  assert(self.engine:start() == "EngineType_1 is roaring")
-  assert(self.transmission:shift() == "TransmissionType_1 is shifting")
-  assert(self.wheel:spin() == "WheelType_1 is spinning")
+  assert(self.chassis:hold() == "ChassisType1 ok")
+  assert(self.engine:start() == "EngineType1 is roaring")
+  assert(self.transmission:shift() == "TransmissionType1 is shifting")
+  assert(self.wheel:spin() == "WheelType1 is spinning")
   return "All components are functional"
 end
 
 --------------------------------------------------------------------------------
--- Car Type #2
+-- Tesla Car Type:
 
-local Tesla = FactoryCar:new {
-  chassis = chassisType_2:new {},
-  engine = engineType_2:new {},
-  transmission = transmissionType_2:new {},
-  wheel = wheelType_2:new {}
+local Tesla = CarFactory:new {
+  chassis = chassisType2:new {},
+  engine = engineType2:new {},
+  transmission = transmissionType2:new {},
+  wheel = wheelType2:new {}
 }
 
 function Tesla:check()
   assert(self.chassis and self.engine and self.transmission and self.wheel)
-  assert(self.chassis:hold() == "ChassisType_2 ok")
-  assert(self.engine:start() == "EngineType_2 is roaring")
-  assert(self.transmission:shift() == "TransmissionType_2 is shifting")
-  assert(self.wheel:spin() == "WheelType_2 is spinning")
+  assert(self.chassis:hold() == "ChassisType2 ok")
+  assert(self.engine:start() == "EngineType2 is roaring")
+  assert(self.transmission:shift() == "TransmissionType2 is shifting")
+  assert(self.wheel:spin() == "WheelType2 is spinning")
   return "All components are functional"
 end
 
 --------------------------------------------------------------------------------
--- #7. Testing the Cars:
+-- Testing the Cars:
 
 Ford:start()       --> self.check: 	true	All components are functional
 Ford:drive("ana")  --> Car is driving: ana
